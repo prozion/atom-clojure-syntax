c0a870c0db7df05009e89b4bca8ee559fdacaec7
Denis Shirshov <denis.shirshov@gmail.com>
2022-02-24 15:55:10 +0300

    3

---
+++ examples/clojure.clj
@@ -0,0 +1,32 @@
+(ns clj-tabtree.tabtree2)
+
+(require '[[clojure.string :as str]])
+
+(def ^:dynamic ns false)
+
+(def a 10)
+
+((fn [x] (* x 2)) 10)
+
+(defn strip-ns [s doowee]
+  (let [astr (str s)]
+    (if (indexof? 'a 10)
+      #"foo\"s"
+      200)))
+      
+(+ 200.5 nil TERIBERKA)
+
+{:foo 100 :bar "acti\"on"}
+
+(+ 10 20)
+(foo "sddd")
+(reduce 1 2 3)
+
+^aot
+
+(cond
+  ((= i 0) 1)
+  ((not= i 10) nil)
+  1e50)
+  
+  
--- examples/racket.rkt
+++ examples/racket.rkt
@@ -4,9 +4,14 @@
 
 (provide (all-defined-out))
 
+(define foo 10)
+
 ; a function
 (define (foobar a b)
   (let* ((c 100))
     (list a b c "some string")))
+
+(foobar 10 20)
+(string 1000 #:a)
 
 (+ 100.5 #t)
